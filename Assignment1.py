5b643227925a70029db430994b940f485b0b809b
rd146354 <rdurrett@msudenver.edu>
2019-02-18 18:48:06 -0700

    Added some lines.

---
+++ Assignment1.py
@@ -0,0 +1,17 @@
+firstNumber = 26
+someString = 'Text goes here.'
+print("Foo" + "Bar")
+computed_num = 3*7+22-8
+some_list = []
+some_list.append("Test data")
+my_dict = {}
+my_dict['appendage001'] = 'leg'
+my_dict['appendage002'] = 'arm'
+print(my_dict['appendage001'])
+type(my_dict)
+type(5)
+type('Testing')
+
+for i in range(10):
+    print(i)
+
